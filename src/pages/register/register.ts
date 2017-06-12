a4fea468a40d27b15478fba35938130a6ad4c249
matheus-lima <matheus.lima@cc.ci.ufpb.br>
2017-06-12 11:39:05 -0300

    Bug SideMenu Fix

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, ToastController } from 'ionic-angular';
+import { NavController, NavParams, ToastController, MenuController } from 'ionic-angular';
 
 import { Credential } from '../../model/credential';
 import { User } from '../../model/user';
@@ -21,18 +21,29 @@ export class RegisterPage {
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
               public toastCtrl: ToastController,
+              public menuCtrl: MenuController,
               public firebaseService: FirebaseService) {
     this.credential = new Credential();
     this.user = new User();
+    this.menuCtrl.enable(false);
   }
 
   ionViewDidLoad() {}
+
+  ionViewWillEnter() {
+    console.log('register enter')
+  }
+
+  ionViewWillLeave() {
+    console.log('register leave')
+  }
 
   registerUser(){
     this.firebaseService.createUserWithCredential(this.user, this.credential, (isSuccess, response) => {
       if (isSuccess) {
         this.presentToast('Conta criada com sucesso!');
         console.log(response);
+        this.menuCtrl.enable(true);
         this.navCtrl.setRoot(HomePage);
       }
       else {
