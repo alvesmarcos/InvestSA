f29e18719a816f982d1a0663917281d21dd956ad
diegguinholima <diego.filipe@cc.ci.ufpb.br>
2017-09-22 15:27:57 -0300

    Refatora Fomulario de  Atendimento

--- src/pages/attendance/attendance.ts
+++ src/pages/attendance/attendance.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController, ViewController } from 'ionic-angular';
+import { Validators, FormBuilder } from '@angular/forms';
 
 import { EmailComposer } from '@ionic-native/email-composer';
+import { GlobalValidator } from '../../model/GlobalValidator';
+
 
 @Component({
   selector: 'page-attendance',
@@ -10,34 +13,34 @@ import { EmailComposer } from '@ionic-native/email-composer';
 
 export class AttendancePage {
   masksPhoneNumber: any;
-
-  //private todo : FormGroup;
-  dataEmail = {
-    fullname: '',
-    Email: '',
-    phone: '',
-    goals: '',
-    date: '',
+  cadastro : any = {};
+  
+  constructor(public navCtrl: NavController, private emailComposer: EmailComposer, private viewCtrl: ViewController, public formBuilder : FormBuilder) {
+    /*this.masksPhoneNumber = ['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/];*/
+    this.cadastro = this.formBuilder.group({
+      nome:['', Validators.required],
+      email:['', Validators.compose([Validators.required, GlobalValidator.mailFormat])],
+      phone:['', Validators.compose([Validators.required, GlobalValidator.phoneFormat])],
+      goal:['', Validators.required],
+      date:['', Validators.required],
+    })
   }
 
-  constructor(public navCtrl: NavController, private emailComposer: EmailComposer, private viewCtrl: ViewController) {
-    this.masksPhoneNumber = ['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/];
-  }
-
-  postDados(req){
-    console.log(req);
+  postDados(){
+    console.log(this.cadastro.value);
   }
 
   sendEmail(){
-    let email = {
+    let mail = {
       to: 'salaacoes@yahoo.com.br', // coloca teu email aqui pra testar
-      cc: this.dataEmail.Email,
+      cc: this.cadastro.value.email,
       subject: 'Agendamento de Consultoria',
-      body: 'Olá meu nome é: '+ this.dataEmail.fullname + ', e eu gostaria de agendar uma consultoria com a sala de ações na seguinte data: '
-      + this.dataEmail.date + '. Favor entrar em contato comigo por email ou pelo telefone: '+ this.dataEmail.phone + '.',
+      body: 'Olá meu nome é: '+ this.cadastro.value.nome + ', e eu gostaria de agendar uma consultoria com a sala de ações com o objetivo de falar sobre os seguintes temas: '+
+      this.cadastro.value.goal + '. Na seguinte data: ' + this.cadastro.value.date + '. Caso haja alguma dúvida sobre o agendamento do atendimento, favor entrar em contato comigo por email ou pelo telefone: '+ this.cadastro.value.phone + '.',
       isHtml: true
     };
-    this.emailComposer.open(email);
+    //console.log(mail);
+    this.emailComposer.open(mail);
   }
 
   closeAttendace() {
